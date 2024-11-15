2b0da713411ac6e261cb8519cbc59f8b69428605
Yogalax-Website <gouravdosad987@gmail.com>
2024-11-15 21:52:29 +0530

    connected MongoDB

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// conenction to mongodb
-mongoose.connect("mongodb://localhost/todo_express", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// connection to mongodb
+const mongoUri = 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+// Connect to MongoDB
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+
+  // middlewares
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static("public"));
+  app.set("view engine", "ejs");
+
+  // routes
+  app.use(require("./routes/index"));
+  app.use(require("./routes/todo"));
+
+  // server configurations
+  app.listen(3000, () => console.log("Server started listening on port: 3000"));
 });
-
-
-// middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
-
-
-// routes
-app.use(require("./routes/index"))
-app.use(require("./routes/todo"))
-
-
-// server configurations....
-app.listen(3000, () => console.log("Server started listening on port: 3000"));
